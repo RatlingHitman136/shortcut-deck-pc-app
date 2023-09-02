e47b8a525f17a24dfe0ecd087d5c26863f59060e
Borys Trehubenko <boris23451@gmail.com>
2023-09-02 15:18:02 +0200

    added profiles and profile manager

---
+++ ShortCutDeckDesktop/ShortCuts/ShortCutProfile.cs
@@ -0,0 +1,36 @@
+﻿using ShortCutDeckDesktop.ShortCuts.ShortCutTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortCutDeckDesktop.ShortCuts
+{
+    public class ShortCutProfile
+    {
+        // short cut, x coord, y coord
+        private List<(ShortCutBase, GridPos)> _shortCuts;
+
+        public ShortCutProfile() {
+            _shortCuts = new List<(ShortCutBase, GridPos)>();
+        }
+
+        public List<(ShortCutBase, GridPos)> ShortCuts { get => _shortCuts; }
+
+        public struct GridPos
+        {
+            private int _x;
+            private int _y;
+
+            public GridPos(int x, int y)
+            {
+                _x = x;
+                _y = y;
+            }
+
+            public int X { get => _x;}
+            public int Y { get => _y;}
+        }
+    }
+}
---
+++ ShortCutDeckDesktop/ShortCuts/ShortCutProfileManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortCutDeckDesktop.ShortCuts
+{
+    public static class ShortCutProfileManager
+    {
+        private static List<ShortCutProfile> _profiles = new List<ShortCutProfile>();
+
+        public static List<ShortCutProfile> Profiles { get => _profiles; }
+    }
+}
