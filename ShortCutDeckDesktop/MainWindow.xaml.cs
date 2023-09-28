75399991057ddd6e6ded6d6492a6f7eebb4ca47d
BORYS-PC\Borys Trehubenko <borys.trehubenko@gmail.com>
2023-09-28 22:02:46 +0200

    variant of notifyIcon implemented

--- ShortCutDeckDesktop/MainWindow.xaml.cs
+++ ShortCutDeckDesktop/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Hardcodet.Wpf.TaskbarNotification;
 using ShortCutDeckDesktop.Networking;
 using ShortCutDeckDesktop.ShortCuts;
 
@@ -22,11 +23,25 @@ namespace ShortCutDeckDesktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TaskbarIcon notifyIcon;
+
         public MainWindow()
         {
             InitializeComponent();
             ShortCutProfileManager.initTestOneProfile();
             Logger.setServerLogField(tb_server_log);
+
+            //notifyIcon = new TaskbarIcon();
+            //notifyIcon.Icon = new System.Drawing.Icon("""Icons\tastbarTray.ico""",128, 128);
+
+            //notifyIcon.TrayMouseDoubleClick += NotifyIcon_TrayMouseDoubleClick;
+
+            //StateChanged += MainWindow_StateChanged;
+        }
+
+        private void NotifyIcon_TrayMouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            Show();
         }
 
         private void btn_start_server_Click(object sender, RoutedEventArgs e)
@@ -37,6 +52,20 @@ namespace ShortCutDeckDesktop
         private void btn_clear_log_field_Click(object sender, RoutedEventArgs e)
         {
             tb_server_log.Text = string.Empty;
+        }
+
+        private void MainWindow_StateChanged(object? sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
         }
     }
 }
