e7b5e470eaf16f19f71de879eaf5708185f68e6c
BORYS-PC\Borys Trehubenko <borys.trehubenko@gmail.com>
2023-10-23 21:59:37 +0200

    finally done proper sized editor grid

---
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/Converters/EditorColumnWidthConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.Converters
+{
+    public class EditorColumnWidthConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double height;             // [0] - ActualHeight
+            double horizontalMargin;   // [1] - used margins horizontally
+            double verticalMargin;     // [2] - used margins vertically
+            int horizontalGridSize;    // [3] - grid size horizontally
+            int verticalGridSize;      // [4] - grid size vertically
+
+            try
+            {
+                height = (double)values[0];
+                horizontalMargin = (double)values[1];
+                verticalMargin = (double)values[2];
+                horizontalGridSize = (int)values[3];
+                verticalGridSize = (int)values[4];
+            }
+            catch { throw new ArgumentException(); }
+
+            double actualGridHeight = height - 2 * verticalMargin;
+            double actualGridWidth = actualGridHeight / verticalGridSize * horizontalGridSize;
+
+            double returnVal = actualGridWidth + 2 * horizontalMargin;
+
+            if (targetType != typeof(GridLength))
+                throw new ArgumentException();
+
+            return new GridLength(returnVal);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
@@ -1,18 +1,32 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ShortCutDeckDesktop.ShortCuts;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.ViewModels
 {
     internal class ShortCutProfileEditorViewModel : ObservableObject
     {
-        private double _widthToHeightRatio = 0.5625;
+        private double _editorHorizontalMargin;
+        private double _editorVerticalMargin;
 
-        public double WidthToHeightRatio { get => _widthToHeightRatio; }
+        public ShortCutProfileEditorViewModel()
+        {
+            _editorHorizontalMargin = 15;
+            _editorVerticalMargin = 15;
+        }
+
+        public int EditorHorizontalGridSize { get => ShortCutProfileManager.GridWidth; }
+        public int EditorVerticalGridSize { get => ShortCutProfileManager.GridHeight; }
+        public Thickness EditorMargin { get => new Thickness(_editorHorizontalMargin, _editorVerticalMargin, EditorHorizontalMargin, EditorVerticalMargin); }
+        public double EditorHorizontalMargin { get => _editorHorizontalMargin; set => _editorHorizontalMargin = value; }
+        public double EditorVerticalMargin { get => _editorVerticalMargin; set => _editorVerticalMargin = value; }
+
     }
 }
