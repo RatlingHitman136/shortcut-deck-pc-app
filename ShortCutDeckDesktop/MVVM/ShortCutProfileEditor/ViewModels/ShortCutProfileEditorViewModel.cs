ab53f29fa14fc20ac22a1bf89b6a5cea674fbecc
Borys Trehubenko <boris23451@gmail.com>
2023-10-23 11:35:00 +0200

    trying to use multi binding to set width according to heigh

---
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.ViewModels
+{
+    internal class ShortCutProfileEditorViewModel:ObservableObject
+    {
+        public int height { get; set; }
+        public int width { get; set; }
+    }
+}
--- ShortCutDeckDesktop/MVVM/ShortCutProfileViewer/ViewModels/ProfileMainViewModel.cs
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileViewer/ViewModels/ProfileMainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.ViewModels;
 using ShortCutDeckDesktop.ShortCuts;
 using System;
 using System.Collections.Generic;
@@ -11,12 +12,16 @@ namespace ShortCutDeckDesktop.MVVM.ShortCutProfileViewer.ViewModels
     internal class ProfileMainViewModel:ObservableObject
     {
         private ShortCutProfile _profile;
+        private ShortCutProfileEditorViewModel _profileEditor;
 
         public ProfileMainViewModel(ShortCutProfile profile)
         {
             _profile = profile;
+            _profileEditor = new ShortCutProfileEditorViewModel();
+            OnPropertyChanged();
         }
 
         public ShortCutProfile Profile { get => _profile; }
+        public ShortCutProfileEditorViewModel ProfileEditor { get => _profileEditor; }
     }
 }
