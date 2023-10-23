89138b1971774f07926bd9454922dd603787cc7b
Borys Trehubenko <boris23451@gmail.com>
2023-10-23 17:54:54 +0200

    trying to make converter to make height half of the width

---
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/Converters/DoubleMultiplicationConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.Converters
+{
+    public class DoubleMultiplicationConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                double res = 1;
+                foreach (var item in values)
+                {
+                    res *= (double)item;
+                }
+                //res += (double)parameter;
+                return res;
+            }
+            catch { throw new Exception(); }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.ViewModels
 {
-    internal class ShortCutProfileEditorViewModel:ObservableObject
+    internal class ShortCutProfileEditorViewModel : ObservableObject
     {
-        public int height { get; set; }
-        public int width { get; set; }
+        private double _widthToHeightRatio = 0.5;
+
+        public double WidthToHeightRatio { get => _widthToHeightRatio; }
     }
 }
