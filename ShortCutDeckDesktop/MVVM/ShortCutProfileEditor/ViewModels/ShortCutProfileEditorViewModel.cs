0f670e6e3adadcc721afedfa9e1d5396ba543657
BORYS-PC\Borys Trehubenko <borys.trehubenko@gmail.com>
2023-10-23 21:37:03 +0200

    done triggering the resizing

--- ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
@@ -1,16 +1,29 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using ShortCutDeckDesktop.ShortCuts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.ViewModels
 {
     internal class ShortCutProfileEditorViewModel:ObservableObject
     {
-        public int height { get; set; }
-        public int width { get; set; }
+        private ICommand _resizeCommand;
+
+        public ShortCutProfileEditorViewModel()
+        {
+            _resizeCommand = new RelayCommand(OnEditorResized);
+        }
+
+        public ICommand ResizeCommand { get => _resizeCommand; }
+
+        public void OnEditorResized()
+        {
+            throw new Exception();
+        }
     }
 }
