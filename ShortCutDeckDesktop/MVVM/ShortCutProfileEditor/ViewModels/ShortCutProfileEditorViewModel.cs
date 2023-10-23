a5b4097a45c3e0d35783e86a879acf212a0cc2a3
BORYS-PC\Borys Trehubenko <borys.trehubenko@gmail.com>
2023-10-23 21:16:38 +0200

    Auto stash before checking out "HEAD"

--- ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ShortCutDeckDesktop.ShortCuts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
