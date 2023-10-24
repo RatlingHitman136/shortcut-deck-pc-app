5afaa02f3cbd1761d41ba45a8002f9263d1e3d8c
Borys Trehubenko <boris23451@gmail.com>
2023-10-24 15:00:45 +0200

    still thinking about drag and drop

--- ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ShortCutProfileEditorViewModel.cs
@@ -16,10 +16,14 @@ namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.ViewModels
         private double _editorHorizontalMargin;
         private double _editorVerticalMargin;
 
+        private ProfilePreviewerViewModel _profilePreviewerViewModel;
+
         public ShortCutProfileEditorViewModel()
         {
             _editorHorizontalMargin = 15;
             _editorVerticalMargin = 15;
+
+            _profilePreviewerViewModel = new ProfilePreviewerViewModel();
         }
 
         public int EditorHorizontalGridSize { get => ShortCutProfileManager.GridWidth; }
@@ -27,6 +31,6 @@ namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.ViewModels
         public Thickness EditorMargin { get => new Thickness(_editorHorizontalMargin, _editorVerticalMargin, EditorHorizontalMargin, EditorVerticalMargin); }
         public double EditorHorizontalMargin { get => _editorHorizontalMargin; set => _editorHorizontalMargin = value; }
         public double EditorVerticalMargin { get => _editorVerticalMargin; set => _editorVerticalMargin = value; }
-
+        public ProfilePreviewerViewModel ProfilePreviewerViewModel { get => _profilePreviewerViewModel; }
     }
 }
