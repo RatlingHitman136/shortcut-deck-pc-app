8a6eaabab4169973b222827c6f1614d1af1054c2
Borys Trehubenko <boris23451@gmail.com>
2023-10-25 14:48:07 +0200

    doing dragging of shortcuts with canvas

---
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/AncestorDependencyObjectFinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor
+{
+    static class AncestorDependencyObjectFinder
+    {
+        public static T FindAncestor<T>(this DependencyObject obj)
+    where T : DependencyObject
+        {
+            DependencyObject tmp = VisualTreeHelper.GetParent(obj);
+            while (tmp != null && !(tmp is T))
+            {
+                tmp = VisualTreeHelper.GetParent(tmp);
+            }
+            return tmp as T;
+        }
+    }
+}
--- ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ProfilePreviewerViewModel.cs
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ProfilePreviewerViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,12 @@ namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.ViewModels
 {
     class ProfilePreviewerViewModel : ObservableObject
     {
+        private ObservableCollection<object> _shortCuts;
 
+        public ProfilePreviewerViewModel()
+        {
+            _shortCuts = new() { 1, 2 };
+        }
+        public ObservableCollection<object> ShortCuts { get => _shortCuts; }
     }
 }
