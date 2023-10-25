fcd706f5e2bb123717982b04d9e59549ed337952
Borys Trehubenko <boris23451@gmail.com>
2023-10-25 13:50:00 +0200

    drag test completed

--- ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ProfilePreviewerViewModel.cs
+++ ShortCutDeckDesktop/MVVM/ShortCutProfileEditor/ViewModels/ProfilePreviewerViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,13 @@ namespace ShortCutDeckDesktop.MVVM.ShortCutProfileEditor.ViewModels
 {
     class ProfilePreviewerViewModel : ObservableObject
     {
+        private ObservableCollection<object> _nodes;
 
+        public ProfilePreviewerViewModel()
+        {
+            _nodes = new() { 1 };
+        }
+
+        public ObservableCollection<object> Nodes { get => _nodes; }
     }
 }
