15ce8bbb1eb7b080e7aba22cbf331b1bafb058c4
BORYS-PC\Borys Trehubenko <borys.trehubenko@gmail.com>
2023-11-23 22:20:54 +0100

    added saving and loading of profiles

---
+++ ShortCutDeckDesktop/DataLoaders/ProfileJsonLoader.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using ShortCutDeckDesktop.ShortCuts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortCutDeckDesktop.DataLoaders
+{
+    public static class ProfileJsonLoader
+    {
+        const string PROPFILE_SAVE_PATH = @"profiles\";
+
+        private static JsonSerializerSettings json_settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All, Formatting = Formatting.Indented };
+
+        public static void SaveProfile(ShortCutProfile profile)
+        {
+            SaveProfile(profile.GetDataHolder());
+        }
+
+        public static void SaveProfile(ShortCutProfileDataHolder dataHolder)
+        {
+            string fileName = PROPFILE_SAVE_PATH + dataHolder.id + ".json";
+
+            string res = JsonConvert.SerializeObject(dataHolder, json_settings);
+
+            try
+            {
+                File.WriteAllText(fileName, res);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex + " Error by saving profile");
+            }
+        }
+
+        public static List<ShortCutProfile> LoadAllProfiles()
+        {
+            List<ShortCutProfileDataHolder> resDataHolders = new List<ShortCutProfileDataHolder>();
+
+            string folderPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\" + PROPFILE_SAVE_PATH;
+
+            try
+            {
+                DirectoryInfo folderInfo = new DirectoryInfo(folderPath);
+                FileInfo[] files = folderInfo.GetFiles();
+
+                foreach (FileInfo file in files)
+                {
+                    if (file.Extension == ".json")
+                    {
+                        resDataHolders.Add(LoadProfile(file.FullName));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex + " Error by loading all profiles from directory");
+            }
+
+            List<ShortCutProfile> res = new List<ShortCutProfile>();
+
+            foreach (var data in resDataHolders)
+            {
+                try
+                {
+                    res.Add(new ShortCutProfile(data));
+                }
+                catch { }
+            }
+
+            return res;
+        }
+
+        public static ShortCutProfileDataHolder LoadProfile(string fullFileName)
+        {
+            try
+            {
+                string res = File.ReadAllText(fullFileName);
+                ShortCutProfileDataHolder data = JsonConvert.DeserializeObject<ShortCutProfileDataHolder>(res, json_settings);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex + " Error by loading profile");
+            }
+        }
+    }
+}
--- ShortCutDeckDesktop/ShortCuts/ShortCutProfileManager.cs
+++ ShortCutDeckDesktop/ShortCuts/ShortCutProfileManager.cs
@@ -1,11 +1,13 @@
 ﻿using Newtonsoft.Json;
 using ShortCutDeckDesktop.Actions.ActionTypes;
 using ShortCutDeckDesktop.Constants;
+using ShortCutDeckDesktop.DataLoaders;
 using ShortCutDeckDesktop.Networking;
 using ShortCutDeckDesktop.ShortCuts.ShortCutTypes;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
 
@@ -13,7 +15,7 @@ namespace ShortCutDeckDesktop.ShortCuts
 {
     public delegate void ShortCutProfilesListUpdateEventHandler(ShortCutProfilesListUpdateEventArgs e);
 
-    public class ShortCutProfilesListUpdateEventArgs:EventArgs
+    public class ShortCutProfilesListUpdateEventArgs : EventArgs
     {
         private List<ShortCutProfile> _newProfilesList;
         public ShortCutProfilesListUpdateEventArgs(List<ShortCutProfile> newProfilesList) => _newProfilesList = newProfilesList;
@@ -26,7 +28,7 @@ namespace ShortCutDeckDesktop.ShortCuts
         private static List<ShortCutProfile> _profiles = new List<ShortCutProfile>();
 
         // TODO values are only for test
-        private static int _gridWidth = 4; 
+        private static int _gridWidth = 4;
         private static int _gridHeight = 6;
         #endregion
 
@@ -45,56 +47,48 @@ namespace ShortCutDeckDesktop.ShortCuts
 
         public static bool TryUpdateExistingProfileWithDataHolder(ShortCutProfileDataHolder dataHolder)
         {
-            if(!_profiles.Exists(x => x.Id == dataHolder.id))
+            if (!_profiles.Exists(x => x.Id == dataHolder.id))
                 return false;
 
             int foundIndex = _profiles.FindIndex(x => x.Id == dataHolder.id);
-            if(foundIndex == -1) return false;
+            if (foundIndex == -1) return false;
 
             ShortCutProfile newProfileInstance = new ShortCutProfile(dataHolder);
             _profiles[foundIndex] = newProfileInstance;
 
             profilesListUpdateEvent(new ShortCutProfilesListUpdateEventArgs(_profiles));
-            SaveProfile(dataHolder);
+            ProfileJsonLoader.SaveProfile(dataHolder);
             NotifyAllClientsProfileChanged(newProfileInstance);
             return true;
         }
 
+        public static void InitProfiles()
+        {
+            _profiles = ProfileJsonLoader.LoadAllProfiles();
+            profilesListUpdateEvent(new ShortCutProfilesListUpdateEventArgs(_profiles));
+        }
+
         private static void NotifyAllClientsProfileChanged(ShortCutProfile profile)
         {
-            foreach(var client in ServerClass.Clients)
+            foreach (var client in ServerClass.Clients)
             {
                 ActionSendProfiles actionSendProfiles = new ActionSendProfiles(client, new List<ShortCutProfile> { profile });
                 actionSendProfiles.ExecuteAction();
             }
         }
 
-        public static void SaveProfile(ShortCutProfileDataHolder profileData)
+        public static void initTestOneProfile()
         {
-            JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All, Formatting = Formatting.Indented };
-            string res = JsonConvert.SerializeObject(profileData, settings);
-            File.WriteAllText("res.json", res);
-            return;
-        }
-
-
-        public static void initTestOneProfile() {
-            string res = File.ReadAllText("res.json");
-            JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All, Formatting = Formatting.Indented };
-            ShortCutProfileDataHolder data = JsonConvert.DeserializeObject<ShortCutProfileDataHolder>(res, settings);
-
-           /* 
-                        ShortCutProfile testProfile = new ShortCutProfile("mainPrf", "Default Profile", 4, 6,
-                            new List<ShortCutBase>
-                            {
+            ShortCutProfile testProfile = new ShortCutProfile("mainPrf", "Default Profile", 4, 6,
+                new List<ShortCutBase>
+                {
                                 new ShortCutButton(new ActionPCVirtualKeyPress(VirtualKeysConstants.VK_VOLUME_UP), 0, 0),
                                 new ShortCutButton(new ActionPCVirtualKeyPress(VirtualKeysConstants.VK_MEDIA_PLAY_PAUSE), 1, 0),
                                 new ShortCutButton(new ActionPCVirtualKeyPress(VirtualKeysConstants.VK_VOLUME_MUTE), 2, 0),
                                 new ShortCutButton(new ActionPCVirtualKeyPress(VirtualKeysConstants.VK_VOLUME_DOWN), 3, 0),
-                            }
-                            );
-                        _profiles.Add(testProfile);*/
-            _profiles.Add(new ShortCutProfile(data));
+                }
+                );
+            _profiles.Add(testProfile);
             profilesListUpdateEvent(new ShortCutProfilesListUpdateEventArgs(_profiles));
         }
     }
